330336c0c27f5d9ce73caf114aefcf2bce90d131
Givi Chapichadze <chapichad@gmail.com>
2024-02-29 13:28:58 +0400

    Update Program.cs

--- StocksApp/Program.cs
+++ StocksApp/Program.cs
@@ -1,6 +1,7 @@
 using Services;
 using ServiceContracts;
 using StocksApp;
+using System.Security.Cryptography;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
